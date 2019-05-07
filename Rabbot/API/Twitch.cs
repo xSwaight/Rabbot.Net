65fc2dd8cec47a2a2c9b7c9b48646e9c00a5e64d
Swaight <p.wellige@ymail.com>
2019-05-07 23:13:42 +0200

    Crash Fix

--- Rabbot/API/Twitch.cs
+++ Rabbot/API/Twitch.cs
@@ -18,19 +18,26 @@ namespace Rabbot.API
         DiscordSocketClient _client;
         public async Task ConfigLiveMonitorAsync(DiscordSocketClient client)
         {
-            _client = client;
-            API = new TwitchAPI();
-            API.Settings.ClientId = Config.bot.twitchToken;
+            try
+            {
+                _client = client;
+                API = new TwitchAPI();
+                API.Settings.ClientId = Config.bot.twitchToken;
 
-            Monitor = new LiveStreamMonitorService(API, 60);
-            List<string> channels = new List<string> {"swaight"};
-            Monitor.SetChannelsByName(channels);
+                Monitor = new LiveStreamMonitorService(API, 60);
+                List<string> channels = new List<string> { "swaight" };
+                Monitor.SetChannelsByName(channels);
 
-            Monitor.OnStreamOnline += Monitor_OnStreamOnline;
-            Monitor.OnStreamOffline += Monitor_OnStreamOffline;
+                Monitor.OnStreamOnline += Monitor_OnStreamOnline;
+                Monitor.OnStreamOffline += Monitor_OnStreamOffline;
 
-            Monitor.Start();
-            await Task.Delay(-1);
+                Monitor.Start();
+                await Task.Delay(-1);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + " " + e.StackTrace);
+            }
         }
 
         private void Monitor_OnStreamOffline(object sender, OnStreamOfflineArgs e)
@@ -46,48 +53,55 @@ namespace Rabbot.API
 
         private async Task StreamOnline(OnStreamOnlineArgs e)
         {
-            var games = await API.Helix.Games.GetGamesAsync(new List<string> { e.Stream.GameId });
-            var users = await API.Helix.Users.GetUsersAsync(new List<string> { e.Stream.UserId });
-
-            var game = games.Games.FirstOrDefault();
-            var user = users.Users.FirstOrDefault();
-
-            using (swaightContext db = new swaightContext())
+            try
             {
-                var dbStream = db.Stream.Where(p => p.StreamId == Convert.ToInt64(e.Stream.Id)).FirstOrDefault();
-                if (dbStream != null)
-                    return;
+                var games = await API.Helix.Games.GetGamesAsync(new List<string> { e.Stream.GameId });
+                var users = await API.Helix.Users.GetUsersAsync(new List<string> { e.Stream.UserId });
 
-                await db.Stream.AddAsync(new Stream { StreamId = Convert.ToInt64(e.Stream.Id), StartTime = e.Stream.StartedAt, Title = e.Stream.Title, TwitchUserId = Convert.ToInt64(e.Stream.UserId) });
-                await db.SaveChangesAsync();
+                var game = games.Games.FirstOrDefault();
+                var user = users.Users.FirstOrDefault();
 
-                foreach (var item in db.Guild)
+                using (swaightContext db = new swaightContext())
                 {
-                    if (item.StreamchannelId != null)
-                    {
-                        var guild = _client.Guilds.Where(p => p.Id == (ulong)item.ServerId).FirstOrDefault();
-                        if (guild == null)
-                            continue;
-                        if (!(guild.Channels.Where(p => p.Id == (ulong)item.StreamchannelId).FirstOrDefault() is SocketTextChannel channel))
-                            continue;
+                    var dbStream = db.Stream.Where(p => p.StreamId == Convert.ToInt64(e.Stream.Id)).FirstOrDefault();
+                    if (dbStream != null)
+                        return;
 
-                        var embed = new EmbedBuilder();
-                        var author = new EmbedAuthorBuilder
+                    await db.Stream.AddAsync(new Stream { StreamId = Convert.ToInt64(e.Stream.Id), StartTime = e.Stream.StartedAt, Title = e.Stream.Title, TwitchUserId = Convert.ToInt64(e.Stream.UserId) });
+                    await db.SaveChangesAsync();
+
+                    foreach (var item in db.Guild)
+                    {
+                        if (item.StreamchannelId != null)
                         {
-                            Name = user.DisplayName,
-                            IconUrl = user.ProfileImageUrl
-                        };
-                        embed.WithAuthor(author);
-                        embed.WithTitle(e.Stream.Title);
-                        embed.WithUrl($"https://www.twitch.tv/{e.Channel}");
-                        embed.WithThumbnailUrl(user.ProfileImageUrl);
-                        embed.AddField("Game", game.Name, true);
-                        embed.AddField("Viewers", e.Stream.ViewerCount, true);
-                        var ThumbnailUrl = e.Stream.ThumbnailUrl.Replace("{width}", "1280").Replace("{height}", "720");
-                        embed.WithImageUrl(ThumbnailUrl);
-                        await channel.SendMessageAsync($"Hi {guild.EveryoneRole.Mention}! Ich bin live auf https://www.twitch.tv/{e.Channel} Schaut mal vorbei :)", false, embed.Build());
+                            var guild = _client.Guilds.Where(p => p.Id == (ulong)item.ServerId).FirstOrDefault();
+                            if (guild == null)
+                                continue;
+                            if (!(guild.Channels.Where(p => p.Id == (ulong)item.StreamchannelId).FirstOrDefault() is SocketTextChannel channel))
+                                continue;
+
+                            var embed = new EmbedBuilder();
+                            var author = new EmbedAuthorBuilder
+                            {
+                                Name = user.DisplayName,
+                                IconUrl = user.ProfileImageUrl
+                            };
+                            embed.WithAuthor(author);
+                            embed.WithTitle(e.Stream.Title);
+                            embed.WithUrl($"https://www.twitch.tv/{e.Channel}");
+                            embed.WithThumbnailUrl(user.ProfileImageUrl);
+                            embed.AddField("Game", game.Name, true);
+                            embed.AddField("Viewers", e.Stream.ViewerCount, true);
+                            var ThumbnailUrl = e.Stream.ThumbnailUrl.Replace("{width}", "1280").Replace("{height}", "720");
+                            embed.WithImageUrl(ThumbnailUrl);
+                            await channel.SendMessageAsync($"Hi {guild.EveryoneRole.Mention}! Ich bin live auf https://www.twitch.tv/{e.Channel} Schaut mal vorbei :)", false, embed.Build());
+                        }
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + " " + ex.StackTrace);
             }
         }
     }
