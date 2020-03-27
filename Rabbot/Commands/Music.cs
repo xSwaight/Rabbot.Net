0b1ebef8296c4f0952529614a00c0976c855b27d
Swaight <p.wellige@ymail.com>
2020-03-27 22:37:35 +0100

    AudioService improvements

--- Rabbot/Commands/Music.cs
+++ Rabbot/Commands/Music.cs
@@ -1,4 +1,6 @@
-﻿using Discord;
+﻿using CliWrap;
+using CliWrap.Buffered;
+using Discord;
 using Discord.Commands;
 using MediaToolkit;
 using MediaToolkit.Model;
@@ -6,6 +8,7 @@ using Rabbot.Services;
 using Serilog;
 using Serilog.Core;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using VideoLibrary;
@@ -40,33 +43,12 @@ namespace Rabbot.Commands
         [Command("play", RunMode = RunMode.Async)]
         public async Task Play([Remainder] string url)
         {
-            var youtube = YouTube.Default;
-            var vid = youtube.GetVideo(url);
-            if (vid == null)
-                return;
-            var song = await GetMp3(vid);
-            await _service.SendAudioAsync(Context.Guild, Context.Channel, song);
-            File.Delete(song);
-        }
-
-
-        public async Task<string> GetMp3(YouTubeVideo video)
-        {
-            var source = AppContext.BaseDirectory;
-            var test = await video.GetBytesAsync();
-            File.WriteAllBytes(source + Helper.RemoveSpecialCharacters(video.FullName), test);
-
-            var inputFile = new MediaFile { Filename = source + Helper.RemoveSpecialCharacters(video.FullName) };
-            var outputFile = new MediaFile { Filename = $"{source + Helper.RemoveSpecialCharacters(video.FullName)}.mp3" };
-
-            using (var engine = new Engine(AppContext.BaseDirectory + "ffmpeg.exe"))
-            {
-                engine.GetMetadata(inputFile);
-
-                engine.Convert(inputFile, outputFile);
-                File.Delete(inputFile.Filename);
-            }
-            return outputFile.Filename;
+            //var youtube = YouTube.Default;
+            //var vid = youtube.GetVideo(url);
+            //if (vid == null)
+            //    return;
+            //var song = await GetMp3(vid);
+            await _service.SendAudioAsync(Context.Guild, Context.Channel, url);
         }
     }
 }
--- Rabbot/Services/AudioService.cs
+++ Rabbot/Services/AudioService.cs
@@ -1,20 +1,51 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
+using CliWrap;
+using CliWrap.Buffered;
 using Discord;
 using Discord.Audio;
+using Discord.WebSocket;
+using Serilog;
 
 namespace Rabbot.Services
 {
     public class AudioService
     {
         private readonly ConcurrentDictionary<ulong, IAudioClient> ConnectedChannels = new ConcurrentDictionary<ulong, IAudioClient>();
+        private readonly DiscordSocketClient _client;
+        private static readonly ILogger _logger = Log.ForContext(Serilog.Core.Constants.SourceContextPropertyName, nameof(AudioService));
+
+        public AudioService(DiscordSocketClient client)
+        {
+            _client = client;
+            _client.VoiceServerUpdated += VoiceServerUpdated;
+            _client.UserVoiceStateUpdated += UserVoiceStateUpdated;
+        }
+
+        private async Task UserVoiceStateUpdated(SocketUser user, SocketVoiceState before, SocketVoiceState after)
+        {
+            if (user.Id == _client.CurrentUser.Id && after.VoiceChannel == null)
+            {
+                if(user is SocketGuildUser guildUser)
+                {
+                    await LeaveAudio(guildUser.Guild);
+                }
+
+            }
+        }
+
+        private async Task VoiceServerUpdated(SocketVoiceServer arg)
+        {
+            var test = arg;
+        }
 
         public async Task JoinAudio(IGuild guild, IVoiceChannel target)
         {
-            IAudioClient client;
-            if (ConnectedChannels.TryGetValue(guild.Id, out client))
+
+            if (ConnectedChannels.ContainsKey(guild.Id))
             {
                 return;
             }
@@ -30,41 +61,46 @@ namespace Rabbot.Services
 
         public async Task LeaveAudio(IGuild guild)
         {
-            IAudioClient client;
-            if (ConnectedChannels.TryRemove(guild.Id, out client))
+            if (ConnectedChannels.TryRemove(guild.Id, out IAudioClient client))
             {
                 await client.StopAsync();
+                client.Dispose();
             }
         }
 
-        public async Task SendAudioAsync(IGuild guild, IMessageChannel channel, string path)
+        public async Task SendAudioAsync(IGuild guild, IMessageChannel channel, string url)
         {
-            if (!File.Exists(path))
+            if (ConnectedChannels.TryGetValue(guild.Id, out IAudioClient client))
             {
-                await channel.SendMessageAsync("File does not exist.");
-                return;
-            }
-            IAudioClient client;
-            if (ConnectedChannels.TryGetValue(guild.Id, out client))
-            {
-                using (var ffmpeg = CreateProcess(path))
+                var streamUrl = GetStreamLink(url).Result.TrimEnd();
                 using (var stream = client.CreatePCMStream(AudioApplication.Music))
                 {
-                    try { await ffmpeg.StandardOutput.BaseStream.CopyToAsync(stream); }
+                    try
+                    {
+                        var argument = $"-hide_banner -loglevel panic -i \"{streamUrl}\" -ac 2 -f s16le -ar 48000 pipe:1";
+                        await Cli.Wrap("ffmpeg.exe").WithArguments(argument).WithStandardOutputPipe(PipeTarget.ToStream(stream)).ExecuteAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Error(ex, $"Error in {nameof(SendAudioAsync)}");
+                    }
                     finally { await stream.FlushAsync(); }
                 }
             }
         }
 
-        private Process CreateProcess(string path)
+        private async Task<string> GetStreamLink(string url)
         {
-            return Process.Start(new ProcessStartInfo
+            var result = await Cli.Wrap("youtube-dl.exe")
+                    .WithArguments($"--format bestaudio[protocol!=http_dash_segments] --youtube-skip-dash-manifest --no-playlist --get-url " + url)
+                    .ExecuteBufferedAsync();
+
+            if (result.ExitCode != 0)
             {
-                FileName = "ffmpeg.exe",
-                Arguments = $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",
-                UseShellExecute = false,
-                RedirectStandardOutput = true
-            });
+                _logger.Error("Something went wrong!");
+                return null;
+            }
+            return result.StandardOutput;
         }
     }
 }
