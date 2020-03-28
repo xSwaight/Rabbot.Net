c343c0e8ea85f950e98ad3a31bf22e9d144e002e
Swaight <p.wellige@ymail.com>
2020-03-28 16:07:30 +0100

    AudioService Linux Support

--- Rabbot/Services/AudioService.cs
+++ Rabbot/Services/AudioService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using CliWrap;
 using CliWrap.Buffered;
@@ -21,7 +22,6 @@ namespace Rabbot.Services
         public AudioService(DiscordSocketClient client)
         {
             _client = client;
-            _client.VoiceServerUpdated += VoiceServerUpdated;
             _client.UserVoiceStateUpdated += UserVoiceStateUpdated;
         }
 
@@ -35,11 +35,6 @@ namespace Rabbot.Services
                 }
 
             }
-        }
-
-        private async Task VoiceServerUpdated(SocketVoiceServer arg)
-        {
-            var test = arg;
         }
 
         public async Task JoinAudio(IGuild guild, IVoiceChannel target)
@@ -78,7 +73,7 @@ namespace Rabbot.Services
                     try
                     {
                         var argument = $"-hide_banner -loglevel panic -i \"{streamUrl}\" -ac 2 -f s16le -ar 48000 pipe:1";
-                        await Cli.Wrap("ffmpeg.exe").WithArguments(argument).WithStandardOutputPipe(PipeTarget.ToStream(stream)).ExecuteAsync();
+                        var test = await Cli.Wrap(GetFilePath("ffmpeg")).WithArguments(argument).WithStandardOutputPipe(PipeTarget.ToStream(stream)).ExecuteAsync();
                     }
                     catch (Exception ex)
                     {
@@ -91,16 +86,71 @@ namespace Rabbot.Services
 
         private async Task<string> GetStreamLink(string url)
         {
-            var result = await Cli.Wrap("youtube-dl.exe")
+            var result = await Cli.Wrap(GetFilePath("youtube-dl"))
                     .WithArguments($"--format bestaudio[protocol!=http_dash_segments] --youtube-skip-dash-manifest --no-playlist --get-url " + url)
                     .ExecuteBufferedAsync();
-
             if (result.ExitCode != 0)
             {
                 _logger.Error("Something went wrong!");
                 return null;
             }
             return result.StandardOutput;
+        }
+
+        private string GetFilePath(string filename)
+        {
+            string directory = AppContext.BaseDirectory;
+            string toolFilepath;
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
+            {
+                toolFilepath = Path.Combine(directory, filename + ".exe");
+
+                if (!File.Exists(toolFilepath))
+                {
+                    var assembly = typeof(AudioService).GetTypeInfo().Assembly;
+                    var type = typeof(AudioService);
+                    var ns = type.Namespace;
+
+                    using (var resourceStream = assembly.GetManifestResourceStream($"{ns}.{filename}.exe"))
+                    using (var fileStream = File.OpenWrite(toolFilepath))
+                    {
+                        resourceStream.CopyTo(fileStream);
+                    }
+                }
+                return toolFilepath;
+            }
+            else if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Linux))
+            {
+                //Check if wkhtmltoimage package is installed on this distro in using which command
+                Process process = Process.Start(new ProcessStartInfo()
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    WorkingDirectory = "/bin/",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    FileName = "/bin/bash",
+                    Arguments = $"which {filename}"
+
+                });
+                string answer = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+
+                if (!string.IsNullOrEmpty(answer) && answer.Contains(filename))
+                {
+                    toolFilepath = filename;
+                    return toolFilepath;
+                }
+                else
+                {
+                    throw new Exception($"{filename} does not appear to be installed on this linux system according to which command;");
+                }
+            }
+            else
+            {
+                //OSX not implemented
+                throw new Exception("OSX Platform not implemented yet");
+            }
         }
     }
 }
